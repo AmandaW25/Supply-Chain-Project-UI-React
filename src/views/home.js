2552195e53b0f17abe4cbaabf162265f34d7f3e8
Amanda Wiggins <59849118+AmandaW25@users.noreply.github.com>
2022-10-04 13:36:10 -0400

    Commit from TeleportHQ

---
+++ src/views/home.js
@@ -0,0 +1,388 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './home.css'
+
+const Home = (props) => {
+  return (
+    <div className="home-container">
+      <Helmet>
+        <title>Supply Chain Project UI</title>
+        <meta property="og:title" content="Supply Chain Project UI" />
+      </Helmet>
+      <div className="home-login">
+        <div className="home-frame1">
+          <div className="home-manager">
+            <img
+              alt="Manager58"
+              src="/playground_assets/manager58-37ri-200h.png"
+              className="home-manager1"
+            />
+          </div>
+          <div className="home-associate">
+            <img
+              alt="Associate59"
+              src="/playground_assets/associate59-mtwi-200h.png"
+              className="home-associate1"
+            />
+          </div>
+          <span className="home-text">
+            <span>Login</span>
+          </span>
+        </div>
+        <div className="home-frame2">
+          <span className="home-text02">
+            <span>Company Name</span>
+          </span>
+        </div>
+      </div>
+      <div className="home-manager-stage1">
+        <div className="home-frame11">
+          <span className="home-text04">
+            <span>Welcome, Manager</span>
+          </span>
+        </div>
+        <div></div>
+        <div className="home-frame3">
+          <div className="home-rectangle1">
+            <img
+              alt="Rectangle1560"
+              src="/playground_assets/rectangle1560-iwwa-700h.png"
+            />
+          </div>
+          <div className="home-ellipse1">
+            <img
+              alt="Ellipse1561"
+              src="/playground_assets/ellipse1561-9har-200h.png"
+            />
+          </div>
+          <img
+            alt="Ellipse7567"
+            src="/playground_assets/ellipse7567-3v-200h.png"
+            className="home-ellipse7"
+          />
+          <img
+            alt="Ellipse10570"
+            src="/playground_assets/ellipse10570-jms-200h.png"
+            className="home-ellipse10"
+          />
+          <img
+            alt="Ellipse16576"
+            src="/playground_assets/ellipse16576-gyt2-200h.png"
+            className="home-ellipse16"
+          />
+          <div className="home-frame4"></div>
+          <span className="home-text06">
+            <span>Stage 1</span>
+          </span>
+          <img
+            alt="Rectangle91110"
+            src="/playground_assets/rectangle91110-sc8d-200h.png"
+            className="home-rectangle9"
+          />
+          <img
+            alt="Line21110"
+            src="/playground_assets/line21110-ha1.svg"
+            className="home-line2"
+          />
+          <img
+            alt="Ellipse15575"
+            src="/playground_assets/ellipse15575-0fed-200h.png"
+            className="home-ellipse15"
+          />
+        </div>
+      </div>
+      <div className="home-menu">
+        <div className="home-frame5">
+          <span className="home-text08">
+            <span> Assembly View</span>
+          </span>
+        </div>
+        <div className="home-frame6">
+          <span className="home-text10">
+            <span> Stages</span>
+          </span>
+        </div>
+        <div className="home-frame7">
+          <span className="home-text12">
+            <span> Parts</span>
+          </span>
+        </div>
+        <div className="home-frame8">
+          <span className="home-text14">
+            <span> Associates</span>
+          </span>
+        </div>
+      </div>
+      <div className="home-manager-stage2">
+        <img
+          alt="Rectangle71182"
+          src="/playground_assets/rectangle71182-1to-200h.png"
+          className="home-rectangle7"
+        />
+        <span className="home-text16">
+          <span>Welcome, Manager</span>
+        </span>
+        <div className="home-frame16">
+          <div className="home-logout-variant2">
+            <img
+              alt="Logout1187"
+              src="/playground_assets/logout1187-2eqb-200h.png"
+              className="home-logout"
+            />
+          </div>
+        </div>
+        <div className="home-frame18">
+          <div className="home-frame17">
+            <div className="home-select-variant2">
+              <img
+                alt="Select1181"
+                src="/playground_assets/select1181-2fno-200h.png"
+                className="home-select"
+              />
+            </div>
+          </div>
+          <img
+            alt="Rectangle81193"
+            src="/playground_assets/rectangle81193-itkb-700h.png"
+            className="home-rectangle8"
+          />
+          <span className="home-text18">
+            <span>Stage 2</span>
+          </span>
+          <div className="home-next-stage-variant2">
+            <img
+              alt="NextStage1199"
+              src="/playground_assets/nextstage1199-r57m-200h.png"
+              className="home-next-stage"
+            />
+          </div>
+          <img
+            alt="Ellipse2562"
+            src="/playground_assets/ellipse2562-onlm-200h.png"
+            className="home-ellipse2"
+          />
+          <img
+            alt="Ellipse11571"
+            src="/playground_assets/ellipse11571-jna-200h.png"
+            className="home-ellipse11 home-ellipse11"
+          />
+          <img
+            alt="Ellipse12572"
+            src="/playground_assets/ellipse12572-1zqw-200h.png"
+            className="home-ellipse12"
+          />
+          <img
+            alt="Ellipse13573"
+            src="/playground_assets/ellipse13573-ps3l-200h.png"
+            className="home-ellipse13"
+          />
+          <img
+            alt="Ellipse17577"
+            src="/playground_assets/ellipse17577-ozw-200h.png"
+            className="home-ellipse17"
+          />
+          <img
+            alt="Ellipse18578"
+            src="/playground_assets/ellipse18578-xgq-200h.png"
+            className="home-ellipse18"
+          />
+          <img
+            alt="Ellipse19579"
+            src="/playground_assets/ellipse19579-kkan-200h.png"
+            className="home-ellipse19"
+          />
+          <img
+            alt="Ellipse20580"
+            src="/playground_assets/ellipse20580-993k-200h.png"
+            className="home-ellipse20"
+          />
+          <img
+            alt="Ellipse22582"
+            src="/playground_assets/ellipse22582-pgbi-200h.png"
+            className="home-ellipse22"
+          />
+          <img
+            alt="Rectangle13122"
+            src="/playground_assets/rectangle13122-50za-200h.png"
+            className="home-rectangle13"
+          />
+          <img
+            alt="Line4123"
+            src="/playground_assets/line4123-ym6f.svg"
+            className="home-line4"
+          />
+          <img
+            alt="Rectangle161223"
+            src="/playground_assets/rectangle161223-usqb-200h.png"
+            className="home-rectangle16"
+          />
+        </div>
+      </div>
+      <div className="home-manager-stage3">
+        <img
+          alt="Rectangle101110"
+          src="/playground_assets/rectangle101110-vqlm-200h.png"
+          className="home-rectangle10"
+        />
+        <span className="home-text20">
+          <span>Welcome, Manager</span>
+        </span>
+        <div className="home-frame19">
+          <div className="home-logout-variant21">
+            <img
+              alt="Logout1111"
+              src="/playground_assets/logout1111-94br-200h.png"
+              className="home-logout1"
+            />
+          </div>
+        </div>
+        <div className="home-frame20">
+          <div className="home-frame21 home-frame21">
+            <div className="home-select-variant21">
+              <img
+                alt="Select1111"
+                src="/playground_assets/select1111-nbml-200h.png"
+                className="home-select1"
+              />
+            </div>
+          </div>
+          <img
+            alt="Rectangle111111"
+            src="/playground_assets/rectangle111111-fm4-700h.png"
+            className="home-rectangle11 home-rectangle11"
+          />
+          <img
+            alt="Rectangle121111"
+            src="/playground_assets/rectangle121111-bgpq-200h.png"
+            className="home-rectangle12"
+          />
+          <img
+            alt="Line31111"
+            src="/playground_assets/line31111-ptx.svg"
+            className="home-line3"
+          />
+          <img
+            alt="Ellipse3563"
+            src="/playground_assets/ellipse3563-odeh.svg"
+            className="home-ellipse3"
+          />
+          <img
+            alt="Ellipse4564"
+            src="/playground_assets/ellipse4564-f5zj-200h.png"
+            className="home-ellipse4"
+          />
+          <img
+            alt="Ellipse5565"
+            src="/playground_assets/ellipse5565-xsfw-200h.png"
+            className="home-ellipse5"
+          />
+          <img
+            alt="Ellipse6566"
+            src="/playground_assets/ellipse6566-zieg-200h.png"
+            className="home-ellipse6"
+          />
+          <img
+            alt="Ellipse8568"
+            src="/playground_assets/ellipse8568-a5vg-200h.png"
+            className="home-ellipse8"
+          />
+          <img
+            alt="Ellipse9569"
+            src="/playground_assets/ellipse9569-jvt-200h.png"
+            className="home-ellipse9"
+          />
+          <img
+            alt="Ellipse14574"
+            src="/playground_assets/ellipse14574-r43-200h.png"
+            className="home-ellipse14"
+          />
+          <img
+            alt="Ellipse21581"
+            src="/playground_assets/ellipse21581-9xb-200h.png"
+            className="home-ellipse21"
+          />
+          <div className="home-previous-stage-variant2">
+            <img
+              alt="PreviousStage1216"
+              src="/playground_assets/previousstage1216-8c52-200h.png"
+              className="home-previous-stage"
+            />
+          </div>
+          <img
+            alt="Rectangle171224"
+            src="/playground_assets/rectangle171224-0tow-200h.png"
+            className="home-rectangle17"
+          />
+          <span className="home-text22">
+            <span> Stage 3</span>
+          </span>
+          <div className="home-next-stage-variant21">
+            <img
+              alt="NextStage1511"
+              src="/playground_assets/nextstage1511-qskb-200h.png"
+              className="home-next-stage1"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="home-logout-confirmation">
+        <div className="home-cancel">
+          <img
+            alt="Cancel1230"
+            src="/playground_assets/cancel1230-dciy-200h.png"
+            className="home-cancel1"
+          />
+        </div>
+        <div className="home-confirm">
+          <img
+            alt="Confirm1231"
+            src="/playground_assets/confirm1231-txlb-200h.png"
+            className="home-confirm1"
+          />
+        </div>
+        <img
+          alt="Rectangle201232"
+          src="/playground_assets/rectangle201232-9xg-200h.png"
+          className="home-rectangle20"
+        />
+        <span className="home-text24">
+          <span> Logout</span>
+        </span>
+        <span className="home-text26">
+          <span>You are about to Logout, are you sure?</span>
+        </span>
+      </div>
+      <div className="home-manager-final-stage">
+        <img
+          alt="Rectangle25152"
+          src="/playground_assets/rectangle25152-xrli-200h.png"
+          className="home-rectangle25"
+        />
+        <span className="home-text28">
+          <span> Welcome, Manager</span>
+        </span>
+        <div className="home-frame26">
+          <img
+            alt="Logout155"
+            src="/playground_assets/logout155-m25r-200h.png"
+            className="home-logout2"
+          />
+        </div>
+        <div className="home-frame27">
+          <img
+            alt="Rectangle26157"
+            src="/playground_assets/rectangle26157-1th9-700h.png"
+            className="home-rectangle26"
+          />
+          <span className="home-text30">
+            <span>Assembly Complete</span>
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
